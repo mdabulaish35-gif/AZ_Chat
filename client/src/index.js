5656231ecd84411607d8b35f2090e9b680657b19
MD Abulaish <mdabulaish35@gmail.com>
2026-02-02 01:28:25 +0530

    Initial commit

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import * as process from 'process';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// Ye line ab imports ke neeche hai, isliye error nahi aayega
+window.process = process;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const cors = require("cors");
+
+app.use(cors());
+
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: [ "GET", "POST" ]
+	}
+});
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded");
+	});
+
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name });
+	});
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal);
+	});
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
