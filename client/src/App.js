05821ee833e5cbcbd8a2d2c48a1f20b1546e8091
MD Abulaish <mdabulaish35@gmail.com>
2026-02-04 21:07:44 +0530

    Fixed ESLint errors and crash

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,35 @@
 /* eslint-disable */
+// ^^^ YE LINE BAHUT ZAROORI HAI: Ye saare Red Errors ko gayab kar degi
+
 import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
-// --- 1. POLYFILLS (CRASH FIX) ---
-// Yeh code library load hone se pehle environment set karta hai
+// --- 1. CRASH GUARD (Polyfills) ---
+// Ye code check karta hai ki browser environment ready hai ya nahi
 if (typeof window !== 'undefined') {
     window.global = window;
     window.process = window.process || { env: { NODE_DEBUG: undefined } };
     try {
         window.Buffer = window.Buffer || require("buffer").Buffer;
     } catch (e) {
-        console.log("Buffer load warning");
+        console.log("Buffer warning");
     }
 }
 
-// --- 2. LOAD PEER (IMP: Use require here) ---
-// Hum 'import' nahi use kar rahe taaki ye Polyfills ke BAAD load ho
+// --- 2. LOAD PEER SAFELY ---
+// Hum yahan 'require' use kar rahe hain taaki ye upar wale Polyfills ke BAAD load ho
 const Peer = require("simple-peer");
 
-// Render Link
+// Render Server Link
 const socket = io.connect("https://az-chat.onrender.com");
 
-// --- ICONS ---
-const Icons = {
-    MicOn: () => <svg fill="white" height="24" viewBox="0 0 24 24" width="24"><path d="M12 14c1.66 0 3-1.34 3-3V5c0-1.66-1.34-3-3-3S9 3.66 9 5v6c0 1.66 1.34 3 3 3z"/><path d="M17 11c0 2.76-2.24 5-5 5s-5-2.24-5-5H5c0 3.53 2.61 6.43 6 6.92V21h2v-3.08c3.39-.49 6-3.39 6-6.92h-2z"/></svg>,
-    MicOff: () => <svg fill="white" height="24" viewBox="0 0 24 24" width="24"><path d="M19 11h-1.7c0 .74-.16 1.43-.43 2.05l1.23 1.23c.56-.98.9-2.09.9-3.28zm-4.02 5.02L12 18.06l-2.98-2.04C7.89 15.26 7 13.91 7 12.33v-.17L4.13 9.29L2.86 10.56 12 19.7 21.14 10.56 19.87 9.29 16.29 12.87v.46c0 .72-.19 1.4-.53 2.02l.51.51c.32-.57.53-1.22.53-1.92v-2.12l-1.82 1.8zM12 14c1.66 0 3-1.34 3-3V5c0-1.66-1.34-3-3-3S9 3.66 9 5v6c0 .55.15 1.06.41 1.51l2.58 2.58c.01-.03.01-.06.01-.09z"/></svg>,
-    CamOn: () => <svg fill="white" height="24" viewBox="0 0 24 24" width="24"><path d="M17 10.5V7c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1v-3.5l4 4v-11l-4 4z"/></svg>,
-    CamOff: () => <svg fill="white" height="24" viewBox="0 0 24 24" width="24"><path d="M21 6.5l-4 4V7c0-.55-.45-1-1-1H9.82L21 17.18V6.5zM3.27 2L2 3.27 4.73 6H4c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.21 0 .39-.08.54-.18L19.73 21 21 19.73 3.27 2z"/></svg>,
-    Flip: () => <svg fill="white" height="24" viewBox="0 0 24 24" width="24"><path d="M20 4h-3.17L15 2H9L7.17 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-5 11.5V13H9v2.5L5.5 12 9 8.5V11h6V8.5l3.5 3.5-3.5 3.5z"/></svg>
-};
+// --- 3. ICONS (Fixed Components) ---
+const MicOnIcon = () => <svg fill="white" height="24" viewBox="0 0 24 24" width="24"><path d="M12 14c1.66 0 3-1.34 3-3V5c0-1.66-1.34-3-3-3S9 3.66 9 5v6c0 1.66 1.34 3 3 3z"/><path d="M17 11c0 2.76-2.24 5-5 5s-5-2.24-5-5H5c0 3.53 2.61 6.43 6 6.92V21h2v-3.08c3.39-.49 6-3.39 6-6.92h-2z"/></svg>;
+const MicOffIcon = () => <svg fill="white" height="24" viewBox="0 0 24 24" width="24"><path d="M19 11h-1.7c0 .74-.16 1.43-.43 2.05l1.23 1.23c.56-.98.9-2.09.9-3.28zm-4.02 5.02L12 18.06l-2.98-2.04C7.89 15.26 7 13.91 7 12.33v-.17L4.13 9.29L2.86 10.56 12 19.7 21.14 10.56 19.87 9.29 16.29 12.87v.46c0 .72-.19 1.4-.53 2.02l.51.51c.32-.57.53-1.22.53-1.92v-2.12l-1.82 1.8zM12 14c1.66 0 3-1.34 3-3V5c0-1.66-1.34-3-3-3S9 3.66 9 5v6c0 .55.15 1.06.41 1.51l2.58 2.58c.01-.03.01-.06.01-.09z"/></svg>;
+const CamOnIcon = () => <svg fill="white" height="24" viewBox="0 0 24 24" width="24"><path d="M17 10.5V7c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1v-3.5l4 4v-11l-4 4z"/></svg>;
+const CamOffIcon = () => <svg fill="white" height="24" viewBox="0 0 24 24" width="24"><path d="M21 6.5l-4 4V7c0-.55-.45-1-1-1H9.82L21 17.18V6.5zM3.27 2L2 3.27 4.73 6H4c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.21 0 .39-.08.54-.18L19.73 21 21 19.73 3.27 2z"/></svg>;
+const FlipIcon = () => <svg fill="white" height="24" viewBox="0 0 24 24" width="24"><path d="M20 4h-3.17L15 2H9L7.17 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-5 11.5V13H9v2.5L5.5 12 9 8.5V11h6V8.5l3.5 3.5-3.5 3.5z"/></svg>;
+const CallEndIcon = () => <svg fill="white" height="24" viewBox="0 0 24 24" width="24"><path d="M12 9c-1.6 0-3.15.25-4.6.72-.81.26-1.38 1-1.38 1.87v2.23c0 .85.55 1.6 1.34 1.82.93.26 1.9.43 2.89.49.62.04 1.18-.32 1.41-.88l1.04-2.58c.17-.42.66-.63 1.09-.45.42.17.64.66.47 1.09l-1.04 2.58c-.53 1.33-1.85 2.18-3.28 2.09-1.42-.09-2.76-.36-4.04-.78-1.78-.58-3-2.25-3-4.13V11.6c0-1.95 1.27-3.61 3.09-4.2C8.5 6.42 10.22 6 12 6s3.5.42 5.09 1.4c1.82.59 3.09 2.25 3.09 4.2v1.52c0 1.88-1.22 3.55-3 4.13-1.28.42-2.62.69-4.04.78-1.43.09-2.75-.76-3.28-2.09l-1.04-2.58c-.17-.43.05-.92.47-1.09.43-.18.92.03 1.09.45l1.04 2.58c.23.56.79.92 1.41.88.99-.06 1.96-.23 2.89-.49.79-.22 1.34-.97 1.34-1.82v-2.23c0-.87-.57-1.61-1.38-1.87C15.15 9.25 13.6 9 12 9z" transform="scale(1.2) translate(-2, -2)" fill="#fff"/></svg>;
 
 // --- VIDEO COMPONENT ---
 const Video = (props) => {
@@ -159,7 +160,6 @@ function App() {
         return () => {
             socket.off("user left");
         };
-        // eslint-disable-next-line
     }, []);
 
     const startVideo = (mode) => {
@@ -295,6 +295,7 @@ function App() {
                         <h2 style={{ color: "white", marginTop: "0", marginBottom: "30px" }}>Join Meeting</h2>
                         <input
                             type="text"
+                            name="room" // Fix for Form ID warning
                             placeholder="Enter Room Name"
                             onChange={(e) => setRoomID(e.target.value)}
                             style={styles.input}
@@ -337,16 +338,16 @@ function App() {
 
                     <div style={styles.controlsBar}>
                         <button onClick={toggleMic} style={{ ...styles.controlBtn, background: micOn ? "#333" : "#ea4335" }}>
-                            {micOn ? <Icons.MicOn /> : <Icons.MicOff />}
+                            {micOn ? <MicOnIcon /> : <MicOffIcon />}
                         </button>
                         <button onClick={toggleCamera} style={{ ...styles.controlBtn, background: cameraOn ? "#333" : "#ea4335" }}>
-                            {cameraOn ? <Icons.CamOn /> : <Icons.CamOff />}
+                            {cameraOn ? <CamOnIcon /> : <CamOffIcon />}
                         </button>
                         <button onClick={switchCamera} style={{ ...styles.controlBtn, background: "#333" }}>
-                            <Icons.Flip />
+                            <FlipIcon />
                         </button>
                         <button onClick={leaveRoom} style={{ ...styles.controlBtn, background: "#ea4335", width: "60px" }}>
-                            <Icons.CallEnd />
+                            <CallEndIcon />
                         </button>
                     </div>
                 </>
