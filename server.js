e18707ab2d1bd35e2a356be5ffc5d293d35546ff
MD Abulaish <mdabulaish35@gmail.com>
2026-02-04 20:25:25 +0530

    Fixed white screen crash and import errors

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ const socketToRoom = {};
 io.on('connection', socket => {
     socket.on("join room", roomID => {
         if (users[roomID]) {
-            // Limit increased to allow more participants if needed
             if (users[roomID].length >= 4) {
                 socket.emit("room full");
                 return;
@@ -25,7 +24,6 @@ io.on('connection', socket => {
         } else {
             users[roomID] = [socket.id];
         }
-        
         socketToRoom[socket.id] = roomID;
         const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
         socket.emit("all users", usersInThisRoom);
@@ -39,14 +37,13 @@ io.on('connection', socket => {
         io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
     });
 
-    // --- IMPORTANT: User Left Logic ---
+    // --- USER LEFT FIX ---
     socket.on('disconnect', () => {
         const roomID = socketToRoom[socket.id];
         let room = users[roomID];
         if (room) {
             room = room.filter(id => id !== socket.id);
             users[roomID] = room;
-            // Broadcast to everyone in the room
             socket.broadcast.to(roomID).emit('user left', socket.id);
         }
     });
